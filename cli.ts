d302a26bcdc7edb65888167bad773609c400f61e
actionchain <actionchain@chainguard.dev>
2026-06-12 05:24:21 +0000

    Update Kesin11/actions-timeline@v2.2.4

---
+++ cli.ts
@@ -0,0 +1,46 @@
+import { Command } from "@cliffy/command";
+import { createMermaid } from "./src/workflow_gantt.ts";
+import { Github, parseWorkflowRunUrl } from "@kesin11/gha-utils";
+
+const { options, args } = await new Command()
+  .name("actions-timeline-cli")
+  .description("Command line tool of actions-timeline")
+  .option("-t, --token <token:string>", "GitHub token. ex: $(gh auth token)")
+  .option(
+    "-o, --output <output:file>",
+    "Output md file path. If not set output to STDOUT. ex: output.md",
+  )
+  .option(
+    "--show-waiting-runner <showWaitingRunner:boolean>",
+    "Show waiting runner time in the timeline. Default: true",
+    { default: true },
+  )
+  .arguments("<url:string>")
+  .parse(Deno.args);
+
+const url = args[0];
+const runUrl = parseWorkflowRunUrl(url);
+
+const host = (runUrl.origin !== "https://github.com")
+  ? runUrl.origin
+  : undefined;
+const client = new Github({ token: options.token, host });
+
+const workflowRun = await client.fetchWorkflowRun(
+  runUrl.owner,
+  runUrl.repo,
+  runUrl.runId,
+  runUrl.runAttempt,
+);
+// const workflowJobs = await client.fetchWorkflowJobs([workflowRun]);
+const workflowJobs = await client.fetchWorkflowRunJobs(workflowRun);
+
+const gantt = createMermaid(workflowRun, workflowJobs, {
+  showWaitingRunner: options.showWaitingRunner,
+});
+
+if (options.output) {
+  await Deno.writeTextFile(options.output, gantt);
+} else {
+  console.log(gantt);
+}
---
+++ src/post.ts
@@ -0,0 +1,47 @@
+import { setTimeout } from "node:timers/promises";
+import process from "node:process";
+import {
+  debug,
+  getBooleanInput,
+  getInput,
+  info,
+  summary,
+} from "npm:@actions/core@1.11.1";
+import * as github from "npm:@actions/github@6.0.1";
+import { createMermaid } from "./workflow_gantt.ts";
+import { Github } from "@kesin11/gha-utils";
+
+const main = async () => {
+  const token = getInput("github-token", { required: true });
+  const showWaitingRunner = getBooleanInput("show-waiting-runner");
+  const client = new Github({ token });
+
+  info("Wait for workflow API result stability...");
+  await setTimeout(1000);
+
+  info("Fetch workflow...");
+  // Currently, @actions/core does not provide runAttempt.
+  // ref: https://github.com/actions/toolkit/pull/1387
+  const runAttempt = process.env.GITHUB_RUN_ATTEMPT
+    ? Number(process.env.GITHUB_RUN_ATTEMPT)
+    : 1;
+  const workflowRun = await client.fetchWorkflowRun(
+    github.context.repo.owner,
+    github.context.repo.repo,
+    github.context.runId,
+    runAttempt,
+  );
+  debug(JSON.stringify(workflowRun, null, 2));
+  info("Fetch workflow_job...");
+  const workflowJobs = await client.fetchWorkflowRunJobs(workflowRun);
+
+  debug(JSON.stringify(workflowJobs, null, 2));
+
+  info("Create gantt mermaid diagram...");
+  const gantt = createMermaid(workflowRun, workflowJobs, { showWaitingRunner });
+  await summary.addRaw(gantt).write();
+  debug(gantt);
+
+  info("Complete!");
+};
+main();
